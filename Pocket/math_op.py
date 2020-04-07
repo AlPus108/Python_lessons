f3298d1f319da69e9ddb1ac8d300d0d8a4e25da0
AlPus108 <alex.pustovoy@gmail.com>
2020-04-07 20:36:54 +0300

    math_op

---
+++ Pocket/math_op.py
@@ -0,0 +1,12 @@
+# Ф-и арифметических операций
+def my_add(x,y):
+    return x + y
+
+def my_sub(x,y):
+    return x - y
+
+def my_mult(x,y):
+    return  x * y
+
+def my_div(x,y):
+    return x / y
