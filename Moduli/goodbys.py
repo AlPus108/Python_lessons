c71a82575a0ccbc47e1af1ae4e6cbb1591a380bc
AlPus108 <alex.pustovoy@gmail.com>
2020-04-11 18:47:20 +0300

    Пример модуля 2

---
+++ Moduli/goodbys.py
@@ -0,0 +1,16 @@
+# 2 - перешли из файла 2_Sobstvennye_moduli
+
+# Создаем здесь несколько простых функций
+
+def say_goodbye():
+    print('Goodbye!')
+
+
+def say_bye_bye():
+    print('Bye-bye!')
+
+
+def say_hasta_la_vista():
+    print('Hasta_la_vista')
+
+# 3 --> greetings
