a5bb4ac1f419f5198746a24c1ec780a656b66684
AlPus108 <alex.pustovoy@gmail.com>
2020-04-11 18:47:42 +0300

    Пример модуля 1

---
+++ Moduli/greetings.py
@@ -0,0 +1,16 @@
+# 3 - перешли из файла goodbys
+
+# Создаем здесь некоторые функции
+
+def say_hello():
+    print('Hello!')
+
+
+def say_hi():
+    print('Hi!')
+
+
+def say_hey_there():
+    print('Hey there!')
+
+# 4 --> 2_Sobstvennye_moduli
